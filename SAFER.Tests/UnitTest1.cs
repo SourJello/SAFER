fbec8b3ac74b2407c9d45384728398dc0d10eb37
SourJello <1290605+SourJello@users.noreply.github.com>
2025-04-11 19:27:29 -0600

    Working Unit test via docker compose command

---
+++ SAFER.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+using SAFER.API.Services;
+
+public class OurSkyServiceTests
+{
+    [Fact]
+    public void CanConstructOurSkyService()
+    {
+        var httpClient = new HttpClient();
+        var service = new OurSkyService(httpClient);
+        Assert.NotNull(service);
+    }
+}
